3a66fe099ddec1d52abd0bd7aa4208ac291cf585
PineappleIG <114992002+PineappleIG@users.noreply.github.com>
2023-01-17 10:43:15 +0300

    Update Gornostaev_Ivan_221_353_lab6.1.cpp

--- Gornostaev_Ivan_221_353_lab6.1.cpp
+++ Gornostaev_Ivan_221_353_lab6.1.cpp
@@ -1,43 +1,59 @@
 ﻿// Gornostaev_Ivan_221_353_lab6.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool stackcheck(string& str)
+{
+    stack <char> s;
+    double len = 0;
+    len = str.length();
+    if ((str[0] == ')' or str[0] == ']' or str[0] == '}'))
+    {
+        return 0;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        
+        if (str[i] == '(' or str[i] == '[' or str[i] == '{')
+        {
+            s.push(str[i]);
+        }
+        if (str[i] == ')')
+        {
+            if (s.top() == '(') s.pop();
+            else return 0;
+        }
+        if (str[i] == ']')
+        {
+            if (s.top() == '[') s.pop();
+            else return 0;
+        }
+        if (str[i] == '}')
+        {
+            if (s.top() == '{') s.pop();
+            else return 0;
+        }
+    }
+    if (s.empty()) return 1;
+    else return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
-
-void task3(int* arr, int len)
-{
-	int* copy_arr = new int[len], q, w, e, t1, t2;
-
-	for (int i = 0; i < len; i++)
-	{
-		copy_arr[i] = arr[i];
-	}
-
-	for (int d = 0; d < len; d++)
-	{
-		q = 0;
-		while (copy_arr[d] != 0)
-		{
-			copy_arr[d] = copy_arr[d] / 10;
-			q = q + copy_arr[d] % 10;
-			copy_arr[d] = copy_arr[d] / 10;
-		}
-		copy_arr[d] = q;
-	}
-
-	for (w = 0; w < len; w++)
-	{
-		for (e = 0; e < len - w - 1; e++)
-		{
-			if (copy_arr[e] > copy_arr[e + 1])
-			{
-				t1 = copy_arr[e + 1];
-				copy_arr[e + 1] = copy_arr[e];
-			}
-		}
-	}
+    string str;
+    cout << "Enter string: ";
+    getline(cin, str);
+    bool check = stackcheck(str);
+    if (check != 0) 
+    {
+        cout << "Good" << endl;
+    }
+    else
+    {
+        cout << "Error" << endl;
+    }
+    return 0;
 }
